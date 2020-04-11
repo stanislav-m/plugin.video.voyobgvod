49bbe573c5b5b04dd5f2d5c233239f556151f6bf
Stanislav M <stanislav.mihaylov@gmail.com>
2020-04-11 22:29:58 +0100

    added epg

--- addon.py
+++ addon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-#from __future__ import unicode_literals
 import sys
+import os
 import threading
 from urllib import urlencode
 from urllib import quote_plus
@@ -8,14 +8,18 @@ from urlparse import parse_qsl
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
+import xbmcvfs
 from bs4 import BeautifulSoup
 from resources.lib.voyo_web_api import *
+from resources.lib.epgprocess import *
 if sys.version_info[0] > 2 or sys.version_info[0] == 2 and sys.version_info[1] >= 7:
     import inputstreamhelper
     tv_only = False
 else:
     tv_only = True
 import uuid
+import json
+import codecs
 
 config_par = ['username', 'password', 'device']
 settings = {}
@@ -133,216 +137,251 @@ class voyobg:
         return self.__api.process_play_url(href)
 
 
-def getSettings():
-    for key in config_par:
-        settings[key] = __addon__.getSetting(key)
-    if len(settings['username']) == 0 or len(settings['password']) == 0:
-        __addon__.openSettings()
-        settings['username'] = __addon__.getSetting('username')
-        settings['password'] = __addon__.getSetting('password')
-    if len(settings['device']) == 0:
-        settings['device'] = uuid.uuid4().hex
-        __addon__.setSetting('device', settings['device'])
-    for key in config_par:
-        settings[key] = __addon__.getSetting(key)
+class voyo_plugin:
+    def __init__(self):
+        self.wrkdir = xbmc.translatePath(__addon__.getAddonInfo('profile')).decode('utf-8')
+        self.voyo = voyobg()
+        self.epg = voyo_epg(self.wrkdir)
+        self.epg.start()
+        loginAttemps = 0
+        self.getSettings()
+        while not self.voyo.login() and loginAttemps < 3:
+            loginAttemps += 1
+            dialog = xbmcgui.Dialog()
+            dialog.ok(u'Грешка', u'Некоректни данни за абонамент!')
+            __addon__.openSettings()
+            self.getSettings()
+        self.epg.join()
 
-voyo = voyobg()
+        logofname = '{0}logos.json'.format(self.wrkdir)
+        with open(logofname, 'r') as f:
+            logostr = f.read()
+            self.logos = json.loads(logostr)
 
-loginAttemps = 0
+        epgfname = '{0}epg.json'.format(self.wrkdir)
+        with codecs.open(epgfname, 'r', 'utf-8') as f:
+            epgs = f.read()
+            self.epg = json.loads(epgs)
 
-getSettings()
-while not voyo.login() and loginAttemps < 3:
-    loginAttemps += 1
-    dialog = xbmcgui.Dialog()
-    dialog.ok(u'Грешка', u'Некоректни данни за абонамент!')
-    __addon__.openSettings()
-    getSettings()
+    def getSettings(self):
+        for key in config_par:
+            settings[key] = __addon__.getSetting(key)
+        if len(settings['username']) == 0 or len(settings['password']) == 0:
+            __addon__.openSettings()
+            settings['username'] = __addon__.getSetting('username')
+            settings['password'] = __addon__.getSetting('password')
+        if len(settings['device']) == 0:
+            settings['device'] = uuid.uuid4().hex
+            __addon__.setSetting('device', settings['device'])
+        for key in config_par:
+            settings[key] = __addon__.getSetting(key)
 
+    def getSavedTV(self):
+        tvdb = os.path.join(self.wrkdir, 'voyotv.json')
+        if xbmcvfs.exists(tvdb):
+            mtime = os.path.getmtime(tvdb)
+            now = time.time()
+            tvdb_exists = True
+        if not tvdb_exists or mtime + 24*60*60 < now: # more that 24 hours - expired
+            return []
+        if tvdb_exists:
+            with open(tvdb, 'r') as fp:
+                tvcont = fp.read()
+                tvitems = json.loads(tvcont)
+                return tvitems
+        return []
 
-def _getListItem(li):
-    return xbmc.getInfoLabel('ListItem.%s' % li).decode('utf-8')
+    def saveTV(self, tvitems):
+        tvdb = os.path.join(self.wrkdir,'voyotv.json')
+        with open(tvdb, 'w') as fp:
+            tvcont = json.dumps(tvitems)
+            fp.write(tvcont)
 
+    def list_categories(self):
+        xbmcplugin.setPluginCategory(_handle, 'Voyobg')
+        xbmcplugin.setContent(_handle, 'videos')
+        categories = self.voyo.sections()
+        for name, link in categories:
+            if tv_only:
+                if link != '/tv-radio/':
+                    continue
+            li = xbmcgui.ListItem(label=name)
+            li.setInfo('video', {'title': name,
+                                        'genre': 'Voyo content',
+                                        'mediatype': 'video'})
+            url = get_url(action='listing_sections', category=link.replace('/', '_'))
+            is_folder = True
+            xbmcplugin.addDirectoryItem(_handle, url, li, is_folder)
+        xbmcplugin.endOfDirectory(_handle)
 
-def list_categories():
-    xbmcplugin.setPluginCategory(_handle, 'Voyobg')
-    xbmcplugin.setContent(_handle, 'videos')
-    categories = voyo.sections()
-    for name, link in categories:
-        if tv_only:
-            if link != '/tv-radio/':
-                continue
-
+    def list_item(self, name, link, img, plot, act_str, meta_inf=None):
+        log('{0} :  {1} - {2}'.format(name, link, img))
         li = xbmcgui.ListItem(label=name)
-        li.setInfo('video', {'title': name,
-                                    'genre': 'Voyo content',
-                                    'mediatype': 'video'})
-        url = get_url(action='listing_sections', category=link.replace('/', '_'))
-        is_folder = True
-        xbmcplugin.addDirectoryItem(_handle, url, li, is_folder)
-    xbmcplugin.endOfDirectory(_handle)
+        art = { 'thumb': img, 'poster': img, 'banner' : img, 'fanart': img }
+        li.setArt(art)
+        info_labels = {'title': name, 'plot': plot}
+        if meta_inf:
+            info_labels.update(meta_inf)
+        li.setInfo('video', info_labels)
+        ctxtmenu = []
+        ctxtmenu.append(('Информация', 'XBMC.Action(Info)'))
+        li.addContextMenuItems(ctxtmenu)
+        dict_url = {'action' : act_str, 'category': link.replace('/', '_'),
+                    'name' : name, 'img' : img, 'plot' : plot, 'link' :link}
+        if meta_inf:
+            dict_url.update(meta_inf)
+        url = getUrl(dict_url)
+        xbmcplugin.addDirectoryItem(_handle, url, li, True)
 
-def list_item(name, link, img, plot, act_str, meta_inf=None):
-    log('{0} :  {1} - {2}'.format(name, link, img))
-    li = xbmcgui.ListItem(label=name)
-    art = { 'thumb': img, 'poster': img, 'banner' : img, 'fanart': img }
-    li.setArt(art)
-    info_labels = {'title': name, 'plot': plot}
-    if meta_inf:
-        info_labels.update(meta_inf)
-
-    li.setInfo('video', info_labels)
-
-    ctxtmenu = []
-    ctxtmenu.append(('Информация', 'XBMC.Action(Info)'))
-    li.addContextMenuItems(ctxtmenu)
-
-    dict_url = {'action' : act_str, 'category': link.replace('/', '_'),
-                'name' : name, 'img' : img, 'plot' : plot, 'link' :link}
-    if meta_inf:
-        dict_url.update(meta_inf)
-    url = getUrl(dict_url)
-    xbmcplugin.addDirectoryItem(_handle, url, li, True)
-
-def list_play_url(name, link, img, plot, meta_inf, play_param):
-    log('{0} - {1}'.format(name, play_param['play_url']))
-    if not (sys.version_info[0] > 2 or sys.version_info[0] == 2 and
-            sys.version_info[1] >= 7):
-        dialog = xbmcgui.Dialog()
-        dialog.ok(
-        u'Грешка',
-        u'Вашето устройство не може да възпроизведе това видео.')
-        return
-
-    device_status()
-    if play_param:
-        headers = "User-agent: stagefright/1.2 (Linux;Android 6.0)"
-        PROTOCOL = 'mpd'
-        DRM = 'com.widevine.alpha'
-        is_helper = inputstreamhelper.Helper(PROTOCOL, drm=DRM)
-        if is_helper.check_inputstream():
-            metaflds = ['genre', 'country', 'rating', 'year', 'duration',
-                        'plot']
-            li = xbmcgui.ListItem(label=name, path=play_param['play_url'])
-            inf_labels = {}
-            for mf in metaflds:
-                if mf in meta_inf:
-                    inf_labels[mf] = meta_inf[mf]
-            li.setInfo(type="Video", infoLabels=inf_labels)
-            li.setArt({'thumb': img, 'icon': img, 'fanart': img})
-            li.setProperty('inputstreamaddon', 'inputstream.adaptive')
-            li.setProperty('inputstream.adaptive.manifest_type', PROTOCOL)
-            li.setProperty('inputstream.adaptive.stream_headers', headers)
-            li.setProperty('inputstream.adaptive.license_type', DRM)
-            licURL = play_param['license_url'] + '||R{SSM}|BJBwvlic'
-            li.setProperty('inputstream.adaptive.license_key', licURL)
-            li.setMimeType('application/dash+xml')
-            li.setProperty("IsPlayable", str(True))
-            xbmcplugin.addDirectoryItem(_handle, play_param['play_url'], li)
-    else:
-        dialog = xbmcgui.Dialog()
-        dialog.ok(
-        u'Грешка',
-        u'Видеото не е налично.')
-
-
-def list_content(params):
-    category = params['category']
-    cat_link = category.replace('_', '/')
-    xbmcplugin.setPluginCategory(_handle, category)
-    xbmcplugin.setContent(_handle, 'videos')
-    if cat_link == '/tv-radio/':
-        content = voyo.tv_radio(cat_link)
-        action_str = 'play_tv'
-        for cont in content:
-            name, link, img = cont
-            list_item(name, link, img, '', action_str)
-    else:
-        ret = voyo.process_page(cat_link)
-        if ret:
-            if len(ret) == 2:
-                action_str = 'listing_sections'
-                content, meta = ret
-                for cont in content:
-                    name, link, img = cont
-                    list_item(name, link, img, '', action_str, meta)
-            else:
-                name, link, img, plot, meta, play_param = ret
-                list_play_url(name, link, img, plot, meta, play_param)
-        else:
+    def list_play_url(self, name, link, img, plot, meta_inf, play_param):
+        log('{0} - {1}'.format(name, play_param['play_url']))
+        if not (sys.version_info[0] > 2 or sys.version_info[0] == 2 and
+                sys.version_info[1] >= 7):
             dialog = xbmcgui.Dialog()
             dialog.ok(
             u'Грешка',
             u'Вашето устройство не може да възпроизведе това видео.')
-
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_NONE)
-    xbmcplugin.endOfDirectory(_handle)
-
-
-def device_status():
-    dialog = xbmcgui.Dialog()
-    while not voyo.check_device():
-        dialog.ok(
-        u'Грешка',
-        u'Достигнал си максималния брой устройства, които могат да ползваш с този абонамент.',
-        u'Моля избери и изтрий устройство, за да продължиш да гледаш.'
-        )
-        devices = voyo.get_devices()
-        dev_lst = []
-        for name1, name2, act_text, dev_id in devices:
-            dev_lst.append('{0} {1} {2} ({3})'.format(name1, name2, act_text, dev_id))
-        i = dialog.select(u'Избери устройство за изтриване:', dev_lst)
-        if not voyo.remove_device(devices[i][3]):
-            dialog.ok(u'Грешка', u'Неуспешно изтриване на устройство.')
-
-def play_tv(params):
-    category = params['category']
-    device_status()
-    metaflds = ['genre', 'country', 'rating', 'year', 'duration',
-                'plot']
-    inf_labels = {}
-    for mf in metaflds:
-        if mf in params:
-            inf_labels[mf] = params[mf]
-
-    play_url = voyo.channel(params['link'])
-    if play_url:
-        headers = "User-agent: stagefright/1.2 (Linux;Android 6.0)"
-        if sys.version_info[0] > 2 or sys.version_info[0] == 2 and sys.version_info[1] >= 7:
-            PROTOCOL = 'hls'
-            is_helper = inputstreamhelper.Helper(PROTOCOL)
+            return
+        if play_param:
+            headers = "User-agent: stagefright/1.2 (Linux;Android 6.0)"
+            PROTOCOL = 'mpd'
+            DRM = 'com.widevine.alpha'
+            is_helper = inputstreamhelper.Helper(PROTOCOL, drm=DRM)
             if is_helper.check_inputstream():
-                li = xbmcgui.ListItem(label=params['name'], path=play_url)
+                metaflds = ['genre', 'country', 'rating', 'year', 'duration',
+                            'plot']
+                li = xbmcgui.ListItem(label=name, path=play_param['play_url'])
+                inf_labels = {}
+                for mf in metaflds:
+                    if mf in meta_inf:
+                        inf_labels[mf] = meta_inf[mf]
                 li.setInfo(type="Video", infoLabels=inf_labels)
-                li.setArt({'thumb': params['img'],
-                           'icon': params['img'],
-                           'fanart': params['img']})
+                li.setArt({'thumb': img, 'icon': img, 'fanart': img})
                 li.setProperty('inputstreamaddon', 'inputstream.adaptive')
                 li.setProperty('inputstream.adaptive.manifest_type', PROTOCOL)
                 li.setProperty('inputstream.adaptive.stream_headers', headers)
+                li.setProperty('inputstream.adaptive.license_type', DRM)
+                licURL = play_param['license_url'] + '||R{SSM}|BJBwvlic'
+                li.setProperty('inputstream.adaptive.license_key', licURL)
+                li.setMimeType('application/dash+xml')
                 li.setProperty("IsPlayable", str(True))
-                xbmc.Player().play(item=play_url, listitem=li)
-            else:
-                log('inputstreamhelper check failed.')
+                xbmcplugin.addDirectoryItem(_handle, play_param['play_url'], li)
         else:
-            li = xbmcgui.ListItem(label=params['name'], path=play_url)
-            li.setInfo(type="Video", infoLabels=inf_labels)
-            li.setArt({'thumb': params['img'],
-                       'icon': params['img'],
-                       'fanart': params['img']})
-            li.setProperty("IsPlayable", str(True))
-            xbmc.Player().play(item=play_url, listitem=li)
+            dialog = xbmcgui.Dialog()
+            dialog.ok(
+            u'Грешка',
+            u'Видеото не е налично.')
 
-def router(paramstring):
-    params = dict(parse_qsl(paramstring))
-    if params:
-        if params['action'] == 'listing_sections':
-            list_content(params)
-        elif params['action'] == 'play_tv':
-            play_tv(params)
+    def list_play_tv_url(self, name, link, img, play_url):
+        tvmapping = {
+            'btv': "bTV", 'int': "bTVi", 'comedy': "bTVComedy",
+            'cinema': "bTVCinema", 'action': "bTVAction", 'lady': "bTVLady",
+            'ring': "RING", 'voyo-cinema':"VoyoCinema"
+        }
+        if play_url:
+            epg_str = ''
+            offset = -2*60*60 # to be defined externally
+            if name in tvmapping:
+                name = tvmapping[name]
+                img = self.logos[name]
+                chan_epg = self.epg[name]
+                cnt = 0
+                now = time.time()
+                for it in chan_epg:
+                    start = time.mktime(
+                        time.strptime(it[0].split()[0],'%Y%m%d%H%M%S'))
+                    start += offset
+                    stop = time.mktime(
+                        time.strptime(it[1].split()[0],'%Y%m%d%H%M%S'))
+                    stop += offset
+                    title = it[2].encode('utf-8')
+                    if (start < now and stop >= now) or (now < start):
+                        cnt += 1
+                        ln = '{0} {1}\n'.format(
+                            time.strftime('%H:%M', time.localtime(start)),
+                                                title)
+                        epg_str += ln
+                    if cnt >= 10:
+                        break
+            li = xbmcgui.ListItem(label=name, path=play_url)
+            li.setInfo(type="Video", infoLabels={'genre':'TV',
+                'plot':epg_str })
+            li.setArt({'thumb': img, 'icon': img, 'fanart': img})
+            li.setProperty("IsPlayable", str(True))
+            if sys.version_info[0] > 2 or sys.version_info[0] == 2 and sys.version_info[1] >= 7:
+                headers = "User-agent: stagefright/1.2 (Linux;Android 6.0)"
+                PROTOCOL = 'hls'
+                is_helper = inputstreamhelper.Helper(PROTOCOL)
+                if is_helper.check_inputstream():
+                    li.setProperty('inputstreamaddon', 'inputstream.adaptive')
+                    li.setProperty('inputstream.adaptive.manifest_type', PROTOCOL)
+                    li.setProperty('inputstream.adaptive.stream_headers', headers)
+                else:
+                    log('inputstreamhelper check failed.')
+            xbmcplugin.addDirectoryItem(_handle, play_url, li)
+
+    def list_content(self, params):
+        self.device_status()
+        category = params['category']
+        cat_link = category.replace('_', '/')
+        xbmcplugin.setPluginCategory(_handle, category)
+        xbmcplugin.setContent(_handle, 'videos')
+        if cat_link == '/tv-radio/':
+            content = self.getSavedTV()
+            if len(content) == 0:
+                content = self.voyo.tv_radio(cat_link)
+                self.saveTV(content)
+            for cont in content:
+                name, link, img, play_url = cont
+                self.list_play_tv_url(name, link, img, play_url)
         else:
-            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
-    else:
-        list_categories()
+            ret = self.voyo.process_page(cat_link)
+            if ret:
+                if len(ret) == 2:
+                    action_str = 'listing_sections'
+                    content, meta = ret
+                    for cont in content:
+                        name, link, img = cont
+                        self.list_item(name, link, img, '', action_str, meta)
+                else:
+                    name, link, img, plot, meta, play_param = ret
+                    self.list_play_url(name, link, img, plot, meta, play_param)
+            else:
+                dialog = xbmcgui.Dialog()
+                dialog.ok(
+                u'Грешка',
+                u'Вашето устройство не може да възпроизведе това видео.')
+        xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_NONE)
+        xbmcplugin.endOfDirectory(_handle)
+
+    def device_status(self):
+        dialog = xbmcgui.Dialog()
+        while not self.voyo.check_device():
+            dialog.ok(
+            u'Грешка',
+            u'Достигнал си максималния брой устройства, които могат да ползваш с този абонамент.',
+            u'Моля избери и изтрий устройство, за да продължиш да гледаш.'
+            )
+            devices = self.voyo.get_devices()
+            dev_lst = []
+            for name1, name2, act_text, dev_id in devices:
+                dev_lst.append('{0} {1} {2} ({3})'.format(name1, name2, act_text, dev_id))
+            i = dialog.select(u'Избери устройство за изтриване:', dev_lst)
+            if not self.voyo.remove_device(devices[i][3]):
+                dialog.ok(u'Грешка', u'Неуспешно изтриване на устройство.')
+
+    def run(self, paramstring):
+        params = dict(parse_qsl(paramstring))
+        if params:
+            if params['action'] == 'listing_sections':
+                self.list_content(params)
+            else:
+                raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+        else:
+            self.list_categories()
 
 if __name__ == '__main__':
-    router(sys.argv[2][1:])
+    p = sys.argv[2][1:]
+    v = voyo_plugin()
+    v.run(p)
+
---
+++ resources/lib/epgprocess.py
@@ -0,0 +1,128 @@
+import xml.etree.cElementTree as etree
+import sys
+import json
+import requests
+import gzip
+import os
+import time
+import threading
+import datetime
+import codecs
+
+epg_url = 'https://epg.kodibg.org/dl.php'
+channel_names = ["bTVi", "bTV", "bTVComedy", "bTVCinema", "bTVAction", "bTVLady", "RING","VoyoCinema"]
+
+class voyo_epg(threading.Thread):
+    def __init__(self, workdir, url=epg_url, chan_set=channel_names):
+        threading.Thread.__init__(self)
+        self.__url = url
+        self.__chan_set = chan_set
+        self.__workdir = workdir
+        if workdir[len(workdir)-1] != '/':
+            self.__workdir += '/'
+
+    def __download(self, chunk_size=128):
+        self.__gzfile = '{0}epg.xml.gz'.format(self.__workdir)
+        attempt = 0
+        while attempt < 5:
+            attempt += 1
+            r = requests.get(self.__url, stream=True)
+            if r.status_code == 200:
+                with open(self.__gzfile, 'wb') as fd:
+                    for chunk in r.iter_content(chunk_size=chunk_size):
+                        fd.write(chunk)
+                return True
+        return False
+
+    def __unpack(self):
+        self.__xmlepg = '{0}epg.xml'.format(self.__workdir)
+        with gzip.open(self.__gzfile, "rb") as f:
+            content = f.read()
+            with open(self.__xmlepg, 'wb') as xmlf:
+                xmlf.write(content)
+
+    def __getLogo(self):
+        logos = {}
+        chan =[]
+        chan.extend(self.__chan_set)
+        name = icon = ''
+        for event, element in etree.iterparse(self.__xmlepg, events=("start", "end")):
+            if len(chan) == 0:
+                break
+            if event == 'end':
+                if element.tag == "channel":
+                    ch = element.attrib['id']
+                    if ch in chan:
+                        logos[ch] = icon
+                        del chan[ch.index(ch)]
+                elif element.tag == 'display-name':
+                    name = element.text
+                elif element.tag == 'icon':
+                    icon = element.attrib['src']
+        return logos
+
+    def __getChannelEpg(self):
+        start = stop = title = desc = icon = ''
+        epg_dict = {}
+        emptylist = []
+        for event, element in etree.iterparse(self.__xmlepg, events=("start", "end")):
+            if event == 'end':
+                if element.tag == "programme":
+                    start = element.attrib['start']
+                    stop = element.attrib['stop']
+                    channel = element.attrib['channel']
+                    if channel in self.__chan_set:
+                        if not (channel in epg_dict):
+                            epg_dict[channel] = []
+                        #epg_dict[channel].append((start, stop, title, desc, icon))
+                        epg_dict[channel].append((start, stop, title, '', icon))
+                    start = stop = title = desc = icon = ''
+                elif element.tag == 'title':
+                    title = element.text
+                elif element.tag == 'desc':
+                    desc = element.text
+                elif element.tag == 'icon':
+                    icon = element.attrib['src']
+        return epg_dict
+
+    def __tidyup(self):
+        os.unlink(self.__xmlepg)
+        os.unlink(self.__gzfile)
+
+    def run(self):
+        epg_exists = False
+        epgfname = '{0}epg.json'.format(self.__workdir)
+        logofname = '{0}logos.json'.format(self.__workdir)
+        if os.path.exists(epgfname):
+            mtime = os.path.getmtime(epgfname)
+            now = time.time()
+            epg_exists = True
+        if not epg_exists or mtime + 24*60*60 < now: # more that 24 hours - expired
+            if self.__download():
+                self.__unpack()
+                logostr = json.dumps(self.__getLogo(), ensure_ascii=False)
+                with open(logofname, 'w') as f:
+                    f.write(logostr)
+                epg_str = json.dumps(self.__getChannelEpg(), ensure_ascii=False)
+                with codecs.open(epgfname, 'w', 'utf-8') as f:
+                    f.write(epg_str)
+                self.__tidyup()
+
+def main():
+    epg = voyo_epg('./')
+    epg.start()
+    epg.join()
+    with codecs.open('epg.json', 'r', 'utf-8') as f:
+        epgs = f.read()
+        epgd = json.loads(epgs)
+        for e in epgd:
+            print('\n==> {0}\n'.format(e))
+            eepglst = epgd[e]
+            for it in eepglst:
+                print('{0} - {1} {2}'.format(
+                    it[0], it[1], it[2].encode('utf-8')))
+
+
+if __name__ == '__main__':
+    main()
+
